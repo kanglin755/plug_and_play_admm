b51ac8af131de0b91fc13e5496b9ea7b9adc2f85
Kang <67895472+kanglin1@users.noreply.github.com>
2022-07-21 00:25:37 +0200

    Add code

---
+++ pnp.py
@@ -0,0 +1,50 @@
+def pnp_admm(
+        measurements, forward, forward_adjoint, denoiser, 
+        step_size=1e-4, num_iter=50, max_cgiter=100, cg_tol=1e-7
+    ):
+    """
+    ADMM plug and play. 
+    """
+    x_h =  forward_adjoint(measurements)
+
+    def conjugate_gradient(A, b, x0, max_iter, tol):
+        """
+        Conjugate gradient method for solving Ax=b
+        """
+        x = x0
+        r = b-A(x)
+        d = r
+        for _ in range(max_iter):
+            z = A(d)
+            rr = torch.sum(r**2)
+            alpha = rr/torch.sum(d*z)
+            x += alpha*d
+            r -= alpha*z
+            if torch.norm(r)/torch.norm(b) < tol:
+                break
+            beta = torch.sum(r**2)/rr
+            d = r + beta*d        
+        return x
+
+    def cg_leftside(x):
+        """
+        Return left side of Ax=b, i.e., Ax
+        """
+        return forward_adjoint(forward(x)) + step_size*x
+
+    def cg_rightside(x):
+        """
+        Returns right side of Ax=b, i.e. b
+        """
+        return x_h + step_size*x
+
+    # Start
+    x = torch.zeros_like(x_h)
+    u = torch.zeros_like(x)
+    v = torch.zeros_like(x)
+    for _ in tqdm(range(num_iter)):
+        b = cg_rightside(v-u)
+        x = conjugate_gradient(cg_leftside, b, x, max_cgiter, cg_tol)
+        v = denoiser(x+u)
+        u += (x - v)
+    return v
---
+++ utils.py
@@ -0,0 +1,32 @@
+def conv2d_from_kernel(kernel, channels, device, stride=1):
+    """
+    Returns nn.Conv2d and nn.ConvTranspose2d modules from 2D kernel, such that 
+    nn.ConvTranspose2d is the adjoint operator of nn.Conv2d
+    Arg:
+        kernel: 2D kernel
+        channels: number of image channels
+    """
+    kernel = kernel/kernel.sum()
+    kernel = kernel.repeat(channels, 1, 1, 1)
+
+    filter = nn.Conv2d(
+        in_channels=channels, out_channels=channels,
+        kernel_size=kernel_size, groups=channels, bias=False, stride=stride,
+        # padding=kernel_size//2
+    )
+    filter.weight.data = kernel
+    filter.weight.requires_grad = False
+
+    filter_adjoint = nn.ConvTranspose2d(
+        in_channels=channels, out_channels=channels,
+        kernel_size=kernel_size, groups=channels, bias=False, stride=stride,
+        # padding=kernel_size//2,
+    )
+    filter_adjoint.weight.data = kernel
+    filter_adjoint.weight.requires_grad = False
+
+    return filter.to(device), filter_adjoint.to(device)
+
+def compute_psnr(img1, img2):
+    mse = torch.mean((img1*255 - img2*255) ** 2)
+    return 20 * torch.log10(255.0 / torch.sqrt(mse))
